cb560eda99fdfe6a35aab7b6fb97cdc9b516d7ae
Supakit <supakit.pti@student.mahidol.edu>
2025-11-11 15:29:48 +0000

    Added the test cases

--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,39 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+    def test_add_positive(self):
+        self.assertEqual(self.calc.add(3, 7), 10)
+
+    def test_add_negative(self):
+        self.assertEqual(self.calc.add(-5, 2), -3)
+
+    # --- subtract() ---   
+    def test_subtract_basic(self):
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+
+    def test_subtract_negative(self):
+        self.assertEqual(self.calc.subtract(-2, -5), 3)
+
+    # --- multiply() ---  
+    def test_multiply_normal(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+
+    def test_multiply_zero(self):
+        self.assertEqual(self.calc.multiply(7, 0), 0)
+
+    # --- divide() ---   
+    def test_divide_basic(self):
+        self.assertEqual(self.calc.divide(8, 2), 4)
+
+    def test_divide_equal_case(self):
+        self.assertEqual(self.calc.divide(4, 4), 1)
+
+    # --- modulo() ---  
+    def test_modulo_basic(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+
+    def test_modulo_equal_case(self):
+        self.assertEqual(self.calc.modulo(6, 3), 0)
 
 if __name__ == '__main__':
     unittest.main()
